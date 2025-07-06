36bae36d4993069afe866b0f6f60d956e62383b2
VishakhaSahu05 <sahuvishakha756@gmail.com>
2025-07-06 15:54:12 +0530

    JSX, functional components, and component composition:

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//sx=>babel transpiled it to React.createElement =>ReactElement js object =>htmlElement(render)
-const jsxHeading = <h1 id ="heading">Namaste-React from jsx</h1>
-console.log(jsxHeading);
+const Title = () =>(
+  <h1 className = "head" tabIndex="5">
+    Namaste-React using JSX
+  </h1>
+)
+const number = 1000;
+//Component composition
+const HeadingComponent = () => (
+  <div id="container">
+    {number}
+    {Title()}
+    <Title/>
+    <Title></Title> {/* all are same */}
+    <h1 className="heading">Namaste-React using Functional Component</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
