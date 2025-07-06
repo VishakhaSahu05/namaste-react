a3998d9a54fc6a24f8ecb9dfd360ad554b27ccea
VishakhaSahu05 <sahuvishakha756@gmail.com>
2025-07-06 14:45:54 +0530

    jsx-transpiled

--- App.js
+++ App.js
@@ -1,29 +1,7 @@
-/* <div id = "parent">
-    <div id = "child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id = "child">
-        <h1>I'm h1 tag</h1> 
-        <h2>I'm h2 tag</h2>
-    </div>
-</div> */
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "I'm h1 tag"),
-      React.createElement("h2", {}, "I'm h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm h1 tag"),
-      React.createElement("h2", {}, "I'm h2 tag"),
-    ])
-  ]
-);
-
-console.log(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+//sx=>babel transpiled it to React.createElement =>ReactElement js object =>htmlElement(render)
+const jsxHeading = <h1 id ="heading">Namaste-React from jsx</h1>
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # NAMASTE - REACT 
+
+
+
+# Parcel
+- DevBuild
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Differential Bundling - support older browsers
+- Diagonostic
+- Error handling
+- HTTPs
+- Tree Shaking Algorithm - remove unused code
+- Diffrent dev and prod bundles
